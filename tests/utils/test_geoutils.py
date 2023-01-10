653b1e27e9e14a5d8804bb6b136f2453df81f0ee
CharlesAuthier <charles.authier93@gmail.com>
2023-01-10 19:33:39 +0000

    add test for fetch tag

--- tests/utils/test_geoutils.py
+++ tests/utils/test_geoutils.py
@@ -11,7 +11,7 @@ from torchgeo.datasets.utils import extract_archive
 from shapely.geometry import MultiPolygon
 
 from dataset.aoi import AOI
-from utils.geoutils import create_new_raster_from_base, bounds_gdf, bounds_riodataset, overlap_poly1_rto_poly2, multi2poly
+from utils.geoutils import create_new_raster_from_base, bounds_gdf, bounds_riodataset, overlap_poly1_rto_poly2, fetch_tag_raster
 from utils.utils import read_csv
 
 
@@ -86,11 +86,11 @@ class TestGeoutils(object):
         label_bounds_box = bounds_gdf(label_gdf)
         raster_bounds_box = bounds_riodataset(raster)
         assert overlap_poly1_rto_poly2(label_bounds_box, raster_bounds_box) == 0.0
-
-    def test_multi2poly(self):
-        """Test the conversion from MultiPolygon to Polygon in a GPKG"""
-        multi_gpkg = "tests/data/new_brunswick_aerial/BakerLake_2017_clipped.gpkg"
-        multi2poly(multi_gpkg, 'BakerLake_2017')
-        df = gpd.read_file(multi_gpkg, layer='BakerLake_2017')
-        have_multi = 'MultiPolygon' in df['geometry'].geom_type.values
-        assert have_multi == False
+        
+    def test_fetch_tag_raster(self):
+        """Test to verify if the function really fetch the right information from the raster"""
+        raster_file = "tests/data/massachusetts_buildings_kaggle/22978945_15_uint8_clipped.tif"
+        with rasterio.open(raster_file) as dest:
+            dest.update_tags(checkpoint='test/path/to/checkpoint.pth')
+        assert fetch_tag_raster(raster_file, 'checkpoint') == 'test/path/to/checkpoint.pth'
+         
